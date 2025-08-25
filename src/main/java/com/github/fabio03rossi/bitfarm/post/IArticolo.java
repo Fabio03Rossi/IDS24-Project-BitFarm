ad5cc479d6d100f20a4671a551f7b2e3afe88e4e
AlbertoCaporaletti <alberto.caporaletti@studenti.unicam.it>
2025-08-25 17:29:18 +0200

    Definizione di Articolo e Prodotto

--- src/main/java/com/github/fabio03rossi/bitfarm/post/IArticolo.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/post/IArticolo.java
@@ -1,5 +1,10 @@
 package com.github.fabio03rossi.bitfarm.post;
 
 public interface IArticolo {
+    public String getNome();
+    public void setNome(String nome);
+    public String getDescrizione();
+    public void setDescrizione(String descrizione);
+    public String getId();
 
 }
--- src/main/java/com/github/fabio03rossi/bitfarm/post/Prodotto.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/post/Prodotto.java
@@ -1,18 +1,56 @@
 package com.github.fabio03rossi.bitfarm.post;
 
-public class Prodotto {
-    private int id;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prodotto implements IArticolo {
+    private final int id;
     private String nome;
     private String descrizione;
     private Double prezzo;
-    private String certificazioni [];
+    private List<String> certificazioni;
 
-public Prodotto(int id, String nome, String descrizione, Double prezzo, String certificazioni[]) {
+public Prodotto(int id, String nome, String descrizione, Double prezzo, List<String> certificazioni) {
     this.id = id;
     this.nome = nome;
     this.descrizione = descrizione;
     this.prezzo = prezzo;
     this.certificazioni = certificazioni;
-
 }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public List<String> getCertificazioni() {
+        return certificazioni;
+    }
+
+    public void setCertificazioni(List<String> certificazioni) {
+        this.certificazioni = certificazioni;
+    }
 }
