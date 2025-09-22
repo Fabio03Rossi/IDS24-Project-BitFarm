f1a2b40230d478a0150faa6a2d2b38877a03a9a1
Fabio03Rossi <fabio03.rossi@studenti.unicam.it>
2025-09-22 21:34:14 +0200

    Modifiche alla classe Utente

--- src/main/java/com/github/fabio03rossi/bitfarm/ApplicazioneBitFarm.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/ApplicazioneBitFarm.java
@@ -1,14 +1,5 @@
 package com.github.fabio03rossi.bitfarm;
 
-import com.github.fabio03rossi.bitfarm.account.AziendaBuilder;
-import com.github.fabio03rossi.bitfarm.account.TipologiaAzienda;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Paint;
-import javafx.stage.Stage;
-
 public class ApplicazioneBitFarm {
 
     public static void main(String[] args) {
--- src/main/java/com/github/fabio03rossi/bitfarm/account/Utente.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/account/Utente.java
@@ -2,13 +2,16 @@ package com.github.fabio03rossi.bitfarm.account;
 
 public class Utente extends Account {
     private String nickname;
-    private int id;
+    private int id = -1;
+
+    public Utente(String nickname, String email, String password) {
+        super(email, password);
+        this.nickname = nickname;
+    }
 
     public Utente(int id, String nickname, String email, String password) {
+        this(nickname, email, password);
         this.id = id;
-        this.nickname = nickname;
-        this.setEmail(email);
-        this.setPassword(password);
     }
 
     public String getNickname() {
