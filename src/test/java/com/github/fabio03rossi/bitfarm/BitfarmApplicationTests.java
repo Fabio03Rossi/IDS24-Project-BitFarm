009419f4a62a3e4721fb07c62b17800065d4b142
darksonic300 <fabiross096@gmail.com>
2025-09-23 16:19:07 +0200

    Aggiunto SpringBoot

---
+++ src/test/java/com/github/fabio03rossi/bitfarm/BitfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabio03rossi.bitfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
