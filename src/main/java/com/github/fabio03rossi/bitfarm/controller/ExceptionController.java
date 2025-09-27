d2988d261453bb34a0af7dc89814c61c0edb5037
Fabio03Rossi <fabio03.rossi@studenti.unicam.it>
2025-09-27 17:39:06 +0200

    Cambiata eccezione per errori di input nei controller

--- src/main/java/com/github/fabio03rossi/bitfarm/controller/ExceptionController.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/controller/ExceptionController.java
@@ -3,9 +3,9 @@ package com.github.fabio03rossi.bitfarm.controller;
 import com.github.fabio03rossi.bitfarm.exception.AccessoNegatoException;
 import com.github.fabio03rossi.bitfarm.exception.CarrelloVuotoException;
 import com.github.fabio03rossi.bitfarm.exception.DatiNonTrovatiException;
-import com.github.fabio03rossi.bitfarm.exception.ErroreInputDatiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +18,8 @@ public class ExceptionController {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(value = ErroreInputDatiException.class)
-    public ResponseEntity<Object> erroreInput(ErroreInputDatiException exception) {
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> erroreInput(MethodArgumentNotValidException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
--- src/main/java/com/github/fabio03rossi/bitfarm/services/EventoService.java
+++ src/main/java/com/github/fabio03rossi/bitfarm/services/EventoService.java
@@ -4,9 +4,9 @@ import com.github.fabio03rossi.bitfarm.contenuto.Evento;
 import com.github.fabio03rossi.bitfarm.contenuto.StatoValidazione;
 import com.github.fabio03rossi.bitfarm.database.DBManager;
 import com.github.fabio03rossi.bitfarm.dto.EventoDTO;
-import com.github.fabio03rossi.bitfarm.exception.ErroreInputDatiException;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Component
 public class EventoService implements IEventoService {
@@ -23,15 +23,11 @@ public class EventoService implements IEventoService {
 
     @Override
     public void eliminaEvento(int id) {
-        if(id == 0) throw new ErroreInputDatiException("ID non valido.");
-
         this.db.cancellaEvento(id);
     }
 
     @Override
     public void modificaEvento(int id, EventoDTO dto) {
-        if(id == 0) throw new ErroreInputDatiException("Dati in input non validi.");
-
         Evento ev = new Evento(dto.nome(), dto.descrizione(), dto.data(), dto.posizione());
         ev.setId(id);
         this.db.updateEvento(ev);
